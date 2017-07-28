8dd01b7128fc01e8c0379685b37cd93c6e68869a
cgrimm013 <cagrimm13@gmail.com>
2017-07-28 10:07:19 -0600

    Add Redux
    Wired up Redux and react-redux into the project

--- React_Full_Project/src/index.js
+++ React_Full_Project/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import { HashRouter, Route, Switch } from 'react-router-dom'
 import { createBrowserHistory } from 'history';
 
+//redux and react-redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+
 // Containers
 import Full from './containers/Full/'
 
@@ -13,15 +18,18 @@ import Page404 from './views/Pages/Page404/'
 import Page500 from './views/Pages/Page500/'
 
 const history = createBrowserHistory();
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render((
-  <HashRouter history={history}>
-    <Switch>
-      <Route exact path="/login" name="Login Page" component={Login}/>
-      <Route exact path="/register" name="Register Page" component={Register}/>
-      <Route exact path="/404" name="Page 404" component={Page404}/>
-      <Route exact path="/500" name="Page 500" component={Page500}/>
-      <Route path="/" name="Home" component={Full}/>
-    </Switch>
-  </HashRouter>
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <HashRouter history={history}>
+      <Switch>
+        <Route exact path="/login" name="Login Page" component={Login}/>
+        <Route exact path="/register" name="Register Page" component={Register}/>
+        <Route exact path="/404" name="Page 404" component={Page404}/>
+        <Route exact path="/500" name="Page 500" component={Page500}/>
+        <Route path="/" name="Home" component={Full}/>
+      </Switch>
+    </HashRouter>
+  </Provider>
 ), document.getElementById('root'))
