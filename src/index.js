53a9e6cd216870f37392986923907eafc1a72e22
cgrimm013 <cagrimm13@gmail.com>
2017-08-14 12:03:03 -0600

    Added polling for the manifold pico eci
    Still needs testing. Authentication should come before the polling even runs.

--- src/index.js
+++ src/index.js
@@ -33,7 +33,11 @@ import rootSaga from './sagas'
 const sagaMiddleware = createSagaMiddleware()
 const history = createBrowserHistory();
 //const createStoreWithMiddleware = applyMiddleware(promise, sagaMiddleware)(createStore);
-const store = createStore(reducers, applyMiddleware(promise, sagaMiddleware))
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(promise, sagaMiddleware)
+)
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render((
--- src/sagas.js
+++ src/sagas.js
@@ -1,17 +1,29 @@
+import { delay } from 'redux-saga';
 import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
 import { retrieveManifoldEci } from './utils/manifoldSDK'
 
-function* fetchEci(action) {
+const START_DELAY_TIME = 0;
+const START_ATTEMPT_NUM = 1;
+const MAX_ATTEMPT_NUM = 3;
+
+function* fetchEci(delayTime, attemptNum, action) {
    try {
+      yield delay(delayTime);
       const result = yield call(retrieveManifoldEci);
-      yield put({type: "FETCH_ECI_SUCCEEDED", result});
-   } catch (e) {
+      console.log("result in fetchEci:",result);
+      if(result.eci){
+        yield put({type: "FETCH_ECI_SUCCEEDED", result});
+      }else if(attemptNum <= MAX_ATTEMPT_NUM){//try again
+        yield* fetchEci(delayTime + 1000, attemptNum + 1, action);
+      }
+   } catch (e) {//catch complete failures
       yield put({type: "FETCH_ECI_FAILED", message: e.message});
+      console.error("Failed attempt to retrieve Manifold Pico eci.", e);
    }
 }
 
 function* watchFetchEci() {
-  yield takeLatest("FETCH_ECI", fetchEci);
+  yield takeLatest("FETCH_ECI", fetchEci, START_DELAY_TIME, START_ATTEMPT_NUM);
 }
 
 export default function*  rootSaga() {
