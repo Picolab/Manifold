f3d1847f72552773933fb2261d305a66573ebe2a
Adam Burdett <burdettadam@gmail.com>
2017-09-12 12:17:54 -0600

    store manifold eci

--- src/reducers/reducer_manifoldEci.js
+++ src/reducers/reducer_manifoldEci.js
@@ -4,6 +4,7 @@ export default function(state = "", action){
   switch (action.type) {
     case "FETCH_ECI_SUCCEEDED":
       console.log("action.result.data in reducer_manifoldEci:",action.result);
+      storeManifoldECI(action.result.data.directives[0].options.eci);
       return action.result.data.directives[0].options.eci;
     default:
       return state;
