ab6a6fd4f6d8098d040754f9d2aceea01bdfd95a
Eric Fortney <eafortney2@gmail.com>
2017-10-12 10:13:13 -0600

    Added UI Elements for Registering and Installing Rulesets

--- src/views/MyThings/Thing.js
+++ src/views/MyThings/Thing.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Dropdown, DropdownMenu, DropdownToggle, DropdownItem, Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
+import { Col, Dropdown, ButtonDropdownMenu, DropdownMenu, ButtonDropdown, DropdownToggle, DropdownItem, Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import {logOut} from '../../utils/AuthService';
+import { Combobox } from 'react-input-enhancements';
 import {createThing,removeThing,updateThing} from '../../utils/manifoldSDK';
 import { connect } from 'react-redux';
 import TestForm from './testForm';
@@ -20,16 +21,26 @@ class Thing extends Component {
   constructor(props) {
     super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.toggleBoth = this.toggleBoth.bind(this);
+    this.toggleSettings = this.toggleSettings.bind(this);
+    this.toggleBothRemove = this.toggleBothRemove.bind(this);
+    this.toggleBothInstall = this.toggleBothInstall.bind(this);
     this.handleRemoveClick = this.handleRemoveClick.bind(this);
+    this.handleInstallRulesetClick = this.handleInstallRulesetClick.bind(this);
     this.toggleRemoveModal = this.toggleRemoveModal.bind(this);
+    this.toggleInstallRulesetModal = this.toggleInstallRulesetModal.bind(this);
+    this.toggleInstallRulesetDropdown = this.toggleInstallRulesetDropdown.bind(this);
     this.injectCode = this.injectCode.bind(this);
 
     this.state = {
       dropdownOpen: false,
+      installRulesetDropdownOpen: false,
       removeModal: false,
-      currentApp: 0
+      installRulesetModal: false,
+      rulesetToInstallName: "",
+      url: "",
+      currentApp: 0,
+      value: "DEFAULT INPUT VAL",
+      options: ["OPT1", "OPT2", "THING3", "WHATEVER", "LOOOOOONG ENTRY"]
     }
   }
 
@@ -44,22 +55,46 @@ class Thing extends Component {
     });
   }
 
+  toggleInstallRulesetModal(){
+    this.setState({
+      installRulesetModal: !this.state.installRulesetModal
+    });
+  }
+
   handleRemoveClick(){
     const nameToDelete = this.props.name;
     this.toggleRemoveModal();
     this.props.dispatch({type: "command", command: removeThing, params: [nameToDelete]});
   }
 
-  toggle() {
+  handleInstallRulesetClick(){
+  const appURL = this.props.url;
+  this.toggleInstallRulesetModal();
+  //this.props.dispatch({type: "command", command: installRuleset, params: [appURL]});
+  }
+
+  toggleSettings() {
     this.setState({
       dropdownOpen: !this.state.dropdownOpen
     });
   }
+  toggleInstallRulesetDropdown(){
+    this.setState({
+      installRulesetDropdownOpen: !this.state.installRulesetDropdownOpen
+    });
+  }
 
-  toggleBoth(){
+  toggleBothRemove(){
     this.setState({
       dropdownOpen: !this.state.dropdownOpen,
       removeModal: !this.state.removeModal
+    });
+  }
+
+  toggleBothInstall(){
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen,
+      installRulesetModal: !this.state.installRulesetModal
     });
   }
 
@@ -89,23 +124,29 @@ class Thing extends Component {
         <div className="card-header">
           {this.props.name}
 
-          <Dropdown className="float-right" isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+          <Dropdown className="float-right" isOpen={this.state.dropdownOpen} toggle={this.toggleSettings}>
             <DropdownToggle
               tag="span"
-              onClick={this.toggle}
+              onClick={this.toggleSettings}
               data-toggle="dropdown"
               aria-expanded={this.state.dropdownOpen} >
 
               <i className="fa fa-cogs float-right fa-lg" />
             </DropdownToggle>
             <DropdownMenu>
-              <DropdownItem onClick={this.toggleBoth}>
-                Delete this Pico
+
+              <DropdownItem onClick={this.toggleBothRemove}>
+                Delete a Pico
                 <i className="fa fa-trash float-right" />
               </DropdownItem>
-              <div onClick={this.toggle}>Custom dropdown item</div>
-              <div onClick={this.toggle}>Custom dropdown item</div>
-              <div onClick={this.toggle}>Custom dropdown item</div>
+
+              <DropdownItem onClick={this.toggleBothInstall}>
+                Install an App
+                <i className="fa fa-cloud-download float-right"/>
+              </DropdownItem>
+
+              <div onClick={this.toggleSettings}>Custom dropdown item</div>
+              <div onClick={this.toggleSettings}>Custom dropdown item</div>
             </DropdownMenu>
           </Dropdown>
         </div>
@@ -118,6 +159,38 @@ class Thing extends Component {
           <ModalFooter>
             <Button color="danger" onClick={this.handleRemoveClick}>Delete Thing</Button>{' '}
             <Button color="secondary" onClick={this.toggleRemoveModal}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+
+        <Modal isOpen={this.state.installRulesetModal} toggle={this.toggleInstallRulesetModal} className={'modal-info'}>
+          <ModalHeader toggle={this.toggleInstallRulesetModal}>Install an App</ModalHeader>
+          <ModalBody>
+            <div className="form-group">
+              <label> Select a ruleset to install:</label>
+              {/*<input type="text" className="form-control" id="url" placeholder="Lord Sauron" onChange={(element) => this.setState({ url: element.target.value})}/>*/}
+
+
+                  <Col xs={6}>
+                    <Combobox defaultValue={this.state.value}
+                              options={this.state.options}
+                              onChange={console.log("SUP HOMIES")}
+                              autosize
+                              autocomplete
+                              placeholder="APP NAME">
+                      {(inputProps, { matchingText, width }) =>
+                        <input type='text' {...inputProps} />
+                      }
+                    </Combobox>
+                  </Col>
+
+
+
+
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="info" onClick={this.handleInstallRulesetClick}>Install it</Button>{' '}
+            <Button color="secondary" onClick={this.toggleInstallRulesetModal}>Cancel</Button>
           </ModalFooter>
         </Modal>
 
