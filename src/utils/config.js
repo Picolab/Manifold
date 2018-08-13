17aab049a5ac9f7c36b18ece973a1046b2dfa644
cgrimm013 <cagrimm13@gmail.com>
2018-08-13 11:04:32 -0600

    Separated Dev config with production config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,3 @@
-//constants
 export const CLIENT_HOST_KEY = "AUTH_CLIENT_HOST";
 export const OWNER_ECI_KEY = 'owner_eci_key';
 export const MANIFOLD_ECI_KEY = 'manifold_eci_key';
@@ -9,9 +8,16 @@ export const CLIENT_KEY_KEY = "client_key";
 export const CALLBACK_URL_KEY = "callback_url";
 export const EVENT_PATH = "sky/event";
 export const FUNCTION_PATH = "sky/cloud";
+export const CLIENT_SECRET = "";
+export const PROTOCOL_KEY = "http_protocol";
+//Production
+export const ROOT_SECURED_DID = "VwvGB9gKoJmnG8LRLsRvzB"; //this is the secured DID to the google ruleset on the root pico
+export const HTTP_PROTOCOL = "https://";
 export const HOST = "manifold.picolabs.io:9090";
 export const MANIFOLD_HOST = "manifold.picolabs.io";
-export const CLIENT_SECRET = "";
-export const HTTP_PROTOCOL = "https://";
-export const PROTOCOL_KEY = "http_protocol";
-export const ROOT_SECURED_DID = "VwvGB9gKoJmnG8LRLsRvzB"; //this is the secured DID to the google ruleset on the root pico
+
+//Development
+// export const ROOT_SECURED_DID = ""; //this is the secured DID to the google ruleset on the root pico
+// export const HTTP_PROTOCOL = "http://";
+// export const HOST = "localhost:8080";
+// export const MANIFOLD_HOST = "localhost:3000";
