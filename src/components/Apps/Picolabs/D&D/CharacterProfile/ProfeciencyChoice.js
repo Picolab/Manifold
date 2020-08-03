e4c78be6ce4a8e353580a19ce94361384d866ccf
Norberto Martinez <nmartinezbyu@gmail.com>
2020-08-03 10:40:40 -0600

    committing so that I can switch branches

--- src/components/Apps/Picolabs/D&D/CharacterProfile/ProfeciencyChoice.js
+++ src/components/Apps/Picolabs/D&D/CharacterProfile/ProfeciencyChoice.js
@@ -3,32 +3,57 @@ import React from 'react';
 class ProfeciencyChoice extends React.Component {
   constructor(props) {
     super(props);
-    let choices = this.props.choices
     this.state = {
       isActive: true,
-      choiceNumber: choices.choice,
-      choices: {}
+      choices: this.props.choices,
+      choose: this.props.choices.choose,
+      choosen: {},
+      chosenNumber: 0,
+      isActive: true
     };
 
   }
 
   choiceHandler(proficiency) {
+    console.log(proficiency.name);
+    console.log(this.state.choosen);
     let name = proficiency.name
     let value = proficiency
-    if(this.state.choices[name]) {
-
+    if(this.state.choosen[name]) {
+      let map = this.state.choosen
+      delete map[name]
+      console.log("map after delete", map);
+      this.setState({
+        choosen: map,
+        chosenNumber: map.size,
+        isActive: true
+      })
     }
     else {
-      this.state.choices[name] = proficiency
+      if(this.state.isActive) {
+        let map = this.state.choosen
+        map[name] = value
+        //Object.assign({}, map)
+        let chosenNumber = map.size
+        console.log("map after insert", map);
+        this.setState({
+          choosen: map,
+          chosenNumber: chosenNumber,
+          isActive: chosenNumber === this.state.choose ? false : true
+        })
+      }
     }
   }
 
   displaySectionList(list) {
     let out = [];
     for (let i in list) {
+      let checked = this.state.choosen[list[i].name] ? true : false
+      let disabled =  !checked && !this.state.isActive ? true : false
       out.push(
         <div key={list[i].name}>
-          <input type="radio" className="profeciencyChoice" />{' '}{list[i].name}
+          <input type="radio" className="profeciencyChoice" onClick={()=>{if(!disabled){this.choiceHandler(list[i]);}}} defaultChecked={checked}/>
+            {' '}{list[i].name.split(":")[1]}
         </div>
       )
     }
@@ -37,11 +62,12 @@ class ProfeciencyChoice extends React.Component {
 
   render() {
     let choices = this.state.choices
-    
+    let choose = this.state.choose
+
     return(
       <div>
         <div className="detailsSection">
-          Profeciency Choice: (Choose {choices.choose})
+          Profeciency Choice: (Choose {choose})
           {this.displaySectionList(choices.from)}
         </div>
       </div>
