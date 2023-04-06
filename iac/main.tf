f866b815b7c7f687605392dc16dbea98da5795e7
Bruce Conrad <bruce_conrad@byu.edu>
2023-04-06 13:37:32 -0600

    Create main.tf

---
+++ iac/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0fcf52bcf5db7b003"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "ManifoldInstance"
+  }
+}
